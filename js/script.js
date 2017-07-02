772afb70d937770776cc7aafc4e608dbce37f4b1
yungpanko <jrjohns@mit.edu>
2017-07-02 10:54:51 -0400

    Done.

--- js/script.js
+++ js/script.js
@@ -1,36 +1,38 @@
-$(document).ready(function() {
+$(document).ready(function () {
 
   lightActive();
   redLight();
   yellowLight();
   greenLight();
 
-  
+
 });
 
-function lightActive(){
-  $('.light').on('click', function(){
-      $(this).toggleClass("active");
-      console.log("background purple")
+function lightActive() {
+  $('.light').on('click', function () {
+    $(this).toggleClass("active");
+    console.log("background purple")
   });
 }
 
-function redLight(){
-  $(".redLight").on('click', function(){
+function redLight() {
+  $(".redLight").on('click', function (event) {
     $(this).toggleClass("red");
+    event.stopPropagation()
   });
 }
 
 
-function yellowLight(){
-  $(".yellowLight").on('click', function(){
+function yellowLight() {
+  $(".yellowLight").on('click', function (event) {
     $(this).toggleClass("yellow");
+    event.stopPropagation()
   });
 }
 
-function greenLight(){
-  $(".greenLight").on('click', function(){
+function greenLight() {
+  $(".greenLight").on('click', function (event) {
     $(this).toggleClass("green");
+    event.stopPropagation();
   });
 }
-
